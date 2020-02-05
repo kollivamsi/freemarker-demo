9f8253b1e99b07dd329e432e3f4de17672b73612
Nicholas Hudson <nhudson@nexient.com>
2020-02-05 11:36:55 -0500

    Added test controller, started refactoring template controller.

--- src/main/java/com/nexient/freemarkerdemo/controller/TestController.java
+++ src/main/java/com/nexient/freemarkerdemo/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.nexient.freemarkerdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TemplateController {
+@RequestMapping("test")
+public class TestController {
 
-	@GetMapping("/template")
+	@GetMapping("/ping")	
 	public String ping() {
 		return "OK";
 	}
