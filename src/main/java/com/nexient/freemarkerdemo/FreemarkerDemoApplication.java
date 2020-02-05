0ed5c1f97050c1317c266b97a694957ca7178465
Nicholas Hudson <nhudson@nexient.com>
2020-02-05 11:10:02 -0500

    Spring Boot application is working; /test/ping endpoint is working.

--- src/main/java/com/nexient/freemarkerdemo/FreemarkerDemoApplication.java
+++ src/main/java/com/nexient/freemarkerdemo/FreemarkerDemoApplication.java
@@ -19,7 +19,7 @@ public class FreemarkerDemoApplication {
 	
 	@Bean
 	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.nexient.freemarker-demo")).build();
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.nexient.freemarkerdemo")).build();
 	}
 
 }
